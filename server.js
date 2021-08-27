27f5775f5c7bceb7182d4a1859404e4ea3b5ce28
miraj <miraj00@gmail.com>
2021-08-27 16:17:20 -0400

    all GET routes functioning

--- server.js
+++ server.js
@@ -18,32 +18,35 @@ app.use(express.json());
 app.use(express.static('public'));
 
 
-//const { notes } = require('../../db/db');
-
 //The following HTML routes should be created:
-  //  GET /notes should return the notes.html file.
+  // 1 >>>  GET /notes should return the notes.html file.
 
   app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
   });
 
-  //  GET * should return the index.html file.
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
-  });
-
+//Link to test in browser : http://localhost:3001/notes
 //--------------------------------------------------------------------------
-//    The following API routes should be created:
-  //  GET /api/notes should read the db.json file and return all saved notes as JSON.
-
-  // const { notes } = require('../../db/db.json');
-  //--------------------------------------------------------------------------
- /*
-  app.get('/api/notes', (req, res) => {
-      let results = notes;
-      res.json(results);
+//The following API routes should be created:
+  // 2 >>>  GET /api/notes should read the db.json file and return all saved notes as JSON.
+  
+   const { notes } = require('./Develop/db/db');
+  
+   app.get('/api/notes', (req, res) => {
+     res.json(notes);
+     res.send(notes);
     });
-*/
+ 
+//Link to test in browser : http://localhost:3001/api/notes    
+//---------------------------------------------------------------------------
+//The following HTML routes should be created:
+// 3 >>>  GET * should return the index.html file.
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+    });
+  
+//Link to test in browser : http://localhost:3001/*
+//-----------------------------------------------------------------------
   //  POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
 
   /*
